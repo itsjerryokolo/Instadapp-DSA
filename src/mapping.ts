164228c892786c6428728ab1c0b7feeb6b0ac1b4
Jerry Okolo <jerrycreative@live.co.uk>
2021-08-04 01:09:49 +0100

    Initial commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -7,61 +7,130 @@ import {
   LogNewMaster,
   LogUpdateMaster
 } from "../generated/InstadappDSA/InstadappDSA"
-import { ExampleEntity } from "../generated/schema"
+import {
+   Account,
+   AccountCreated,
+   NewAccount,
+   NewCheck,
+   NewMaster,
+   UpdateMaster
+   } from "../generated/schema"
 
 export function handleLogAccountCreated(event: LogAccountCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let accountCreated =  AccountCreated.load(event.params.account.toHexString() + "-" + "ACCOUNTCREATED")
+  let account = Account.load(event.params.owner.toHexString())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if(account == null){
+    account = new Account(event.params.owner.toHexString() )
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  if(accountCreated == null){
+    accountCreated = new AccountCreated(event.params.account.toHexString() + "-" + "ACCOUNTCREATED")
+  }
 
-  // Entity fields can be set based on event parameters
-  entity.sender = event.params.sender
-  entity.owner = event.params.owner
+  accountCreated.account = event.params.account.toHexString()
+  accountCreated.origin = event.params.origin.toHexString()
+  accountCreated.owner = event.params.owner.toHexString()
+  accountCreated.sender = event.params.sender.toHexString()
+  accountCreated.txHash = event.transaction.hash
+  accountCreated.timestamp = event.block.timestamp
+  accountCreated.BlockNumber = event.block.number
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.account(...)
-  // - contract.build(...)
-  // - contract.check(...)
-  // - contract.connectors(...)
-  // - contract.isClone(...)
-  // - contract.list(...)
-  // - contract.master(...)
-  // - contract.versionCount(...)
+  accountCreated.save()
+  account.save()
 }
 
-export function handleLogNewAccount(event: LogNewAccount): void {}
+export function handleLogNewAccount(event: LogNewAccount): void {
+  let newAccount =  NewAccount.load(event.params._newAccount.toHexString() + "-" + "NEWACCOUNT")
+  let account = Account.load(event.params._newAccount.toHexString())
 
-export function handleLogNewCheck(event: LogNewCheck): void {}
+  if(account == null){
+    account = new Account(event.params._newAccount.toHexString())
+  }
 
-export function handleLogNewMaster(event: LogNewMaster): void {}
+  if(newAccount == null){
+    newAccount = new NewAccount(event.params._newAccount.toHexString() + "-" + "NEWACCOUNT")
+  }
 
-export function handleLogUpdateMaster(event: LogUpdateMaster): void {}
+  newAccount.newAccount= event.params._newAccount.toHexString()
+  newAccount.connectors = event.params._connectors.toHexString()
+  newAccount.check = event.params._check.toHexString()
+  newAccount.txHash = event.transaction.hash
+  newAccount.timestamp = event.block.timestamp
+  newAccount.BlockNumber = event.block.number
+
+
+  newAccount.save()
+  account.save()
+
+}
+
+export function handleLogNewCheck(event: LogNewCheck): void {
+  let newCheck =  NewCheck.load(event.params.check.toHexString() + "-" + "NEWCHECK")
+  let account = Account.load(event.params.check.toHexString())
+
+  if(account == null){
+    account = new Account(event.params.check.toHexString())
+  }
+
+  if(newCheck == null){
+    newCheck = new NewCheck(event.params.check.toHexString() + "-" + "NEWCHECK")
+  }
+
+  newCheck.check = event.params.check.toHexString()
+  newCheck.accountVersion = event.params.accountVersion
+  newCheck.txHash = event.transaction.hash
+  newCheck.timestamp = event.block.timestamp
+  newCheck.BlockNumber = event.block.number
+
+
+  newCheck.save()
+  account.save()
+
+
+}
+
+export function handleLogNewMaster(event: LogNewMaster): void {
+  let newMaster =  NewMaster.load(event.params.master.toHexString() + "-" + "NEWMASTER")
+  let account = Account.load(event.params.master.toHexString())
+
+  if(account == null){
+    account = new Account(event.params.master.toHexString())
+  }
+
+  if(newMaster == null){
+    newMaster = new NewMaster(event.params.master.toHexString() + "-" + "NEWMASTER")
+  }
+
+  newMaster.newMasterAccount = event.params.master.toHexString()
+  newMaster.txHash = event.transaction.hash
+  newMaster.timestamp = event.block.timestamp
+  newMaster.BlockNumber = event.block.number
+
+
+  newMaster.save()
+  account.save()
+}
+
+export function handleLogUpdateMaster(event: LogUpdateMaster): void {
+  let updateMaster =  UpdateMaster.load(event.params.master.toString() + "-" + "UPDATEMASTER")
+  let account = Account.load(event.params.master.toHexString())
+
+  if(account == null){
+    account = new Account(event.params.master.toHexString())
+  }
+
+  if(updateMaster == null){
+    updateMaster = new UpdateMaster(event.params.master.toString() + "-" + "UPDATEMASTER")
+  }
+
+  updateMaster.updatedAccount = event.params.master.toHexString()
+  updateMaster.txHash = event.transaction.hash
+  updateMaster.timestamp = event.block.timestamp
+  updateMaster.BlockNumber = event.block.number
+
+
+  updateMaster.save()
+  account.save()
+}
